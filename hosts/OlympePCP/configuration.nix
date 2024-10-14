dc1304b07b2f7212cee4255cce02f8f2399c3151
Olympe-J <olympe.jouslin@gmail.com>
2024-10-14 16:22:47 +0200

    +steam

--- hosts/OlympePCP/configuration.nix
+++ hosts/OlympePCP/configuration.nix
@@ -57,6 +57,27 @@
     wget
     curl
   ];
+
+
+  # ===== JAVA =====
+  programs.java.enable = true; 
+
+
+  # ===== Steam =====
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    
+    # package = pkgs.steam.override {
+    #   withPrimus = true;
+    #   extraPkgs = pkgs: [ bumblebee glxinfo ];
+    #   withJava = true;
+    # };
+  };
+
+
   # Set the default editor to vim
   environment.variables.EDITOR = "vim";
 
