23c046d5455d37c66923d1fe7fe6e803f2fb183e
Olympe-J <olympe.jouslin@gmail.com>
2024-12-11 14:31:01 +0100

    modification of wallpapers path

--- home/desktop/hyprland/theme.py
+++ home/desktop/hyprland/theme.py
@@ -6,9 +6,19 @@ import os
 
 def main(args):
 
+    defaut_wp = "~/.config/waypaper/wallpapers/evelynn-kda-3840x2160.jpg"
+    red1_wp = "~/.config/waypaper/wallpapers/wallpaper-red1.jpeg"
+    red2_wp = "~/.config/waypaper/wallpapers/wallpaper-red2.jpeg"
+    orange1_wp = "~/.config/waypaper/wallpapers/macos-sequoia-orange.jpg"
+    orange2_wp = "~/.config/waypaper/wallpapers/wallpaper-orange2.png"
+    yellow1_wp = "~/.config/waypaper/wallpapers/wallpaper-yellow1.png"
+    green1_wp = "~/.config/waypaper/wallpapers/wallpaper-green1.png"
+    purple1_wp = "~/.config/waypaper/wallpapers/madeline-trans-purple.jpg"
+
     if len(args) == 2:
 
-        color = ""
+        new_wallpaper = ""
+        new_color = ""
 
         match args[1]:
 
@@ -28,60 +38,57 @@ def main(args):
                 os.system('hyprctl dispatch -- exec "[workspace 2] NickvisionCavalier.GNOME" > /dev/null')
                 os.system('hyprctl dispatch -- exec "[workspace 1] firefox https://music.youtube.com/" > /dev/null')
                 os.system('hyprctl reload > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/evelynn-kda-3840x2160.jpg > /dev/null')
+                os.system('waypaper --wallpaper ' + defaut_wp + ' > /dev/null')
                 return
             
             case "red" | "r":
-                color = "rgba(FF0000FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/wallpaper-red1.jpeg > /dev/null')
+                new_color = "rgba(FF0000FF) rgba(000000FF) 90deg"
+                new_wallpaper = red1_wp
 
             case "red2" | "r2":
-                color = "rgba(FF0000FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/wallpaper-red2.png > /dev/null')
+                new_color = "rgba(FF0000FF) rgba(000000FF) 90deg"
+                new_wallpaper = red2_wp
 
             case "orange" | "o":
-                color = "rgba(FF8000FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/macos-sequoia-orange.jpg > /dev/null')
+                new_color = "rgba(FF8000FF) rgba(000000FF) 90deg"
+                new_wallpaper = orange1_wp
 
             case "orange2" | "o2":
-                color = "rgba(FF8000FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/wallpaper-orange2.png > /dev/null')
+                new_color = "rgba(FF8000FF) rgba(000000FF) 90deg"
+                new_wallpaper = orange2_wp
 
             case "yellow" | "y":
-                color = "rgba(FFFF00FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
+                new_color = "rgba(FFFF00FF) rgba(000000FF) 90deg"
+                new_wallpaper = yellow1_wp
 
             case "green" | "g":
-                color = "rgba(00FF00FF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/wallpaper-green1.png > /dev/null')
+                new_color = "rgba(00FF00FF) rgba(000000FF) 90deg"
+                new_wallpaper = green1_wp
 
             case "cyan" | "c":
-                color = "rgba(00FFFFFF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
+                new_color = "rgba(00FFFFFF) rgba(000000FF) 90deg"
 
             case "blue" | "b":
-                color = "rgba(0000FFFF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
+                new_color = "rgba(0000FFFF) rgba(000000FF) 90deg"
 
             case "purple" | "p":
-                color = "rgba(8000FFFF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/madeline-trans-purple.jpg > /dev/null')
+                new_color = "rgba(8000FFFF) rgba(000000FF) 90deg"
+                new_wallpaper = purple1_wp
                 
             case "purple2" | "p2":
-                color = "rgba(FF00FFFF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
-                os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/evelynn-kda-3840x2160.jpg > /dev/null')
+                new_color = "rgba(FF00FFFF) rgba(000000FF) 90deg"
+                new_wallpaper = defaut_wp
 
             case "pink":
-                color = "rgba(FF80FFFF) rgba(000000FF) 90deg"
-                os.system('hyprctl keyword general:col.active_border "' + color + '" > /dev/null')
+                new_color = "rgba(FF80FFFF) rgba(000000FF) 90deg"
             
+
+        if (new_color != ""):
+            os.system('hyprctl keyword general:col.active_border "' + new_color + '" > /dev/null')
+
+        if (new_wallpaper != ""):
+            os.system('waypaper --wallpaper ' + new_wallpaper + ' > /dev/null')
+
 
         os.system('hyprctl keyword general:border_size 3 > /dev/null')
 
@@ -93,7 +100,7 @@ def main(args):
 
     else:
         os.system('hyprctl reload > /dev/null')
-        os.system('waypaper --wallpaper /etc/nixos/home/desktop/wallpaper/nixosWallpapers/evelynn-kda-3840x2160.jpg > /dev/null')
+        os.system('waypaper --wallpaper ' + defaut_wp + ' > /dev/null')
 
 
 if __name__ == "__main__":
--- home/desktop/wallpaper/default.nix
+++ home/desktop/wallpaper/default.nix
@@ -5,6 +5,12 @@
   # For configuration file of swww
   home.file.".config/waypaper/config.ini".source = ./config_waypaper.ini;
 
+  # For wallpapers
+  home.file.".config/waypaper/wallpapers/" = {
+    source = ./nixosWallpapers;
+    recursive = true;
+  };
+
   # For themewp command
   home.file.".config/waypaper/themewp.py".source = ./themewp.py;
 
